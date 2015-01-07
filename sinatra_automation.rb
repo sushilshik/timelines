a82f23c072c6f8783b65c65f24f037b0d8e128b6
sushilshik <ahundov@gmail.com>
2015-01-07 19:43:35 +0200

    encoding fix

--- sinatra_automation.rb
+++ sinatra_automation.rb
@@ -1,12 +1,11 @@
-#encoding: utf-8
+# encoding: UTF-8
 require 'sinatra'
 require 'cgi'
 
 get '/timeline' do
   datesFileName = CGI.unescape(params[:file])
   line = !params[:line].nil? ? params[:line] : ""
-  line = CGI.unescape(line)
-  `ruby timeline.rb #{datesFileName} "#{line}"`
+  `ruby timeline.rb #{datesFileName} "#{line.encode("utf-8")}"`
 end
 
 # vim: tabstop=4 softtabstop=0 noexpandtab shiftwidth=4 number
--- timeline.rb
+++ timeline.rb
@@ -1,4 +1,4 @@
-#encoding: utf-8
+#encoding: UTF-8
 require 'fileutils'
 
 datesFileName = ARGV[0]
@@ -6,25 +6,23 @@ datesFileName = ARGV[0]
 filename = "timeline.jsx"
 text = File.read(filename) 
 puts = text.gsub(/	datesFileName: ".*",/, "	datesFileName: \""+datesFileName+"\",")
-File.open(filename, "w") { |file| file << puts }
+File.open(filename, "w:UTF-8") { |file| file << puts }
 
-appendLine = ARGV[1]
+appendLine = ARGV[1].dup.force_encoding("cp1251").encode("cp866")
 
 if appendLine != nil && appendLine.length > 0
-	open(datesFileName, 'a') { |f|
-	f.puts appendLine
-}
+	File.open(datesFileName, "a+:UTF-8") { |file| file.puts appendLine }
 end
 
 pattern = Regexp.new("^#timelineWebName:(.*)$")
 datesFile = File.read(datesFileName)
 timelineWebName = !(match_data = datesFile.match(pattern)).nil? ? match_data[1] : ""
 
-system('C:\Program Files\Adobe\Adobe Illustrator CC 2014 (32 Bit)\Support Files\Contents\Windows\Illustrator.exe','C:\Users\mike\Desktop\timeline\timeline.jsx')
+#system('C:\Program Files\Adobe\Adobe Illustrator CC 2014 (32 Bit)\Support Files\Contents\Windows\Illustrator.exe','C:\Users\mike\Desktop\timeline\timeline.jsx')
 
-FileUtils.move(timelineWebName+".pdf", "z:\\")
+#FileUtils.move(timelineWebName+".pdf", "z:\\")
 
 p timelineWebName
 
-#File.open("log.txt", "a") { |file| file << appendLine+"\n" }
+#File.open("log.txt", "a+") { |file| file << line }
 # vim: tabstop=4 softtabstop=0 noexpandtab shiftwidth=4 number
