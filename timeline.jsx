b76053340e93864006af4878a04561ad26c041d4
sushilshik <ahundov@gmail.com>
2015-01-07 15:14:00 +0200

    encoding fix

--- timeline.jsx
+++ timeline.jsx
@@ -14,7 +14,7 @@ var conf = {
 	templateFileName: "template.ai",
 	timelineResultFileName: null,
 	timelineWebName: null,
-	datesFileName: "1930-1947.txt",
+	datesFileName: "1913-1924.txt",
 	templateObjectsLayerName: "",
 	yearScaleTemplateObjectName: "",
 	eventTemplateObjectName: "",
--- timeline.rb
+++ timeline.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 require 'fileutils'
 
 datesFileName = ARGV[0]
