963cd5284bb66761d5b705c68efd32018dc09ab7
sushilshik <ahundov@gmail.com>
2015-01-10 18:57:41 +0200

    timeline titles and comments

--- sinatra_automation.rb
+++ sinatra_automation.rb
@@ -5,7 +5,7 @@ require 'cgi'
 get '/timeline' do
   datesFileName = CGI.unescape(params[:file])
   line = !params[:line].nil? ? params[:line] : ""
-  `ruby timeline.rb #{datesFileName} "#{line.encode("utf-8")}"`
+  `ruby timeline.rb #{datesFileName} "#{line.encode("utf-8")}" "move_pdf"`
 end
 
 # vim: tabstop=4 softtabstop=0 noexpandtab shiftwidth=4 number
--- timeline.rb
+++ timeline.rb
@@ -1,16 +1,19 @@
 #encoding: UTF-8
 require 'fileutils'
 
+pdf_goal_dir = "z:\\"
+
+illustrator_exe = 'C:\Program Files\Adobe\Adobe Illustrator CC 2014 (32 Bit)\Support Files\Contents\Windows\Illustrator.exe'
+timeline_jsx = 'timeline.jsx'
+
 datesFileName = ARGV[0]
 
-filename = "timeline.jsx"
-text = File.read(filename) 
-puts = text.gsub(/	datesFileName: ".*",/, "	datesFileName: \""+datesFileName+"\",")
-File.open(filename, "w:UTF-8") { |file| file << puts }
+text = File.read(timeline_jsx) 
+t = text.gsub(/	datesFileName: ".*",/, "	datesFileName: \""+datesFileName+"\",")
+File.open(timeline_jsx, "w:UTF-8") { |file| file << t } 
 
-appendLine = ARGV[1].dup.force_encoding("cp1251").encode("cp866")
-
-if appendLine != nil && appendLine.length > 0
+if ARGV[1] != nil && ARGV[1].length > 0
+	appendLine = ARGV[1].dup.force_encoding("cp1251").encode("cp866")
 	File.open(datesFileName, "a+:UTF-8") { |file| file.puts appendLine }
 end
 
@@ -18,11 +21,12 @@ pattern = Regexp.new("^#timelineWebName:(.*)$")
 datesFile = File.read(datesFileName)
 timelineWebName = !(match_data = datesFile.match(pattern)).nil? ? match_data[1] : ""
 
-system('C:\Program Files\Adobe\Adobe Illustrator CC 2014 (32 Bit)\Support Files\Contents\Windows\Illustrator.exe','C:\Users\mike\Desktop\timeline\timeline.jsx')
+system(illustrator_exe, timeline_jsx)
 
-FileUtils.move(timelineWebName+".pdf", "z:\\")
+if ARGV[2] != nil && ARGV[2].lenght > 0 && ARGV[2] == "move_pdf" && File.directory?(pdf_goal_dir)
+	FileUtils.move(timelineWebName+".pdf", pdf_goal_dir)
+end
 
 p timelineWebName
 
-#File.open("log.txt", "a+") { |file| file << line }
 # vim: tabstop=4 softtabstop=0 noexpandtab shiftwidth=4 number
